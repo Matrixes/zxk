ca4a6b123ceeeaf7cbf08d516111a43ca5fda311
Durant <batbike@163.com>
2016-10-06 22:43:58 +0800

    delete static/*

--- static/post.js
+++
@@ -1,6 +0,0 @@
-$("#likes").one('click', function() {
-	var postid = $(this).attr("data-postid");
-	$.get("/blog/post_like/", {post_id: postid}, function(data){
-		$('#like_count').html(data);
-	});
-})
