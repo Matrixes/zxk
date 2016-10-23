4e50daf939430beed07a1b5eb38f5102458966e8
Durant <batbike@163.com>
2016-10-23 20:52:11 +0800

    following system is ok

--- accounts/static/accounts/user.js
+++ accounts/static/accounts/user.js
@@ -1,8 +1,9 @@
 /* user follow ajax */
-$(function () {
+
+$(function() {
 	$('a.follow').on('click', function(e) {
 		e.preventDefault();
-		$.post('/accounts/user/follow/', 
+		$.post('/accounts/users/follow/',
 			{
 				id: $(this).data('id'),
 				action: $(this).data('action')
@@ -10,19 +11,19 @@ $(function () {
 			function(data) {
 				if (data['status'] == 'ok') {
 					var previous_action = $('a.follow').data('action');
-					// toggle data-action
+					//toggle data-action
 					$('a.follow').data('action',
 						previous_action == 'follow' ? 'unfollow': 'follow');
 					//toggle link text
 					$('a.follow').text(
-						previous_action == 'follow' ? 'Unfollow' : 'Follow');
+						previous_action == 'follow' ? '取消关注' : '关注');
 					// update total followers
-					var previous_followers = parseInt(
-						$('span.count .total').text());
-						$('span.count .total').text(previous_action == 'follow' ? previous_followers + 1 : previous_followers - 1);
-					)
-				}
-			});
-			
+					var previous_followers = parseInt($('#follower-count').text());
+					$('#follower-count').text(previous_action == 'follow' ? previous_followers + 1 : previous_followers - 1);
+				} 
+			}
+			)
 	})
+
 })
+
