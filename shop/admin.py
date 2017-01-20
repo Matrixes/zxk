07a89683b4dbaf26e6d057f09e2beb8bf169c9c2
Durant <batbike@163.com>
2017-01-20 15:57:10 +0800

    start order

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Order, OrderItem
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -17,3 +17,16 @@ class ProductAdmin(admin.ModelAdmin):
 	prepopulated_fields = {'slug': ['name']}
 
 admin.site.register(Product, ProductAdmin)
+
+
+class OrderItemInline(admin.TabularInline):
+	model = OrderItem
+	raw_id_fields = ['product']
+
+class OrderAdmin(admin.ModelAdmin):
+	list_display = ['id', 'name', 'email', 'address', 
+					'postal_code', 'city', 'paid', 'created', 'updated']
+	list_filter = ['paid', 'created', 'updated']
+	inlines = [OrderItemInline]
+
+admin.site.register(Order, OrderAdmin)
